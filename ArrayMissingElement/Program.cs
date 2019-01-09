1a025cefb5b47bd1683b650c13ba7c4b7a0de44d
Chris Tacey-Green <chris@taceygreen.com>
2019-01-09 12:05:07 +0000

    Add project files.

---
+++ ArrayMissingElement/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+// you can also use other imports, for example:
+// using System.Collections.Generic;
+
+// you can write to stdout for debugging purposes, e.g.
+// Console.WriteLine("this is a debug message");
+
+class Solution
+{
+    public int solution(int[] A)
+    {
+        // write your code in C# 6.0 with .NET 4.5 (Mono)
+        HashSet<int> numberExists = new HashSet<int>();
+        foreach(var item in A)
+        {
+            numberExists.Add(item);
+        }
+        for(int i = 1; i <= A.Length + 1; i++)
+        {
+            if(!numberExists.Contains(i))
+            {
+                return i;
+            }
+        }
+        return 0;
+    }
+}
